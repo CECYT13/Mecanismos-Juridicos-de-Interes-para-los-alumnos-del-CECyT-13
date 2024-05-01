0a2b937a8c96c4533c92d24f4c96791bce430078
CECYT13 <167667304+CECYT13@users.noreply.github.com>
2024-04-30 18:07:29 -0600

    Delete WINDOW.js

--- WINDOW.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var miDiv = document.getElementById('miDiv');
-    if (window.scrollY > 50) { // Cambia este valor según sea necesario para activar el movimiento
-        miDiv.classList.add('scroll-down');
-    } else {
-        miDiv.classList.remove('scroll-down');
-    }
-});
